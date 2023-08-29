56d77f5b518e768a0908c9f578d7d74c6da0a1b8
viseshagarwal <viseshagarwal@outlook.com>
2023-08-29 10:59:54 +0530

    edited few files

--- Lab_Assignment_5/convert_employee_data.py
+++ Lab_Assignment_5/convert_employee_data.py
@@ -1,22 +1,38 @@
 from lxml import etree
+import xmlschema
 
-# Load XML
-xml_tree = etree.parse("C:\Users\vises\OneDrive\Desktop\WSD\Lab_Assignment_5\input.xml")
 
-# Load XSL
-xsl_transform = etree.XSLT(etree.parse("C:\Users\vises\OneDrive\Desktop\WSD\Lab_Assignment_5\transform.xsl"))
+try:
+    xml_file = "Web Stack\Assignment 5\employee_data.xml"
+    xsd_file = "Web Stack\Assignment 5\employee_schema.xsd"
+    xsl_file = "Web Stack\Assignment 5\employee_transform.xsl"
 
-# Apply XSLT transformation
-html_tree = xsl_transform(xml_tree)
+    validator = xmlschema.XMLSchema(xsd_file)
+    if validator.is_valid(xml_file):
+        print("XML file is valid.")
+    else:
+        print("XML file is not valid.")
+        print(validator.validate(xml_file))
 
-# Validate against XSD
-xmlschema = etree.XMLSchema(etree.parse("C:\Users\vises\OneDrive\Desktop\WSD\Lab_Assignment_5\schema.xsd"))
-if xmlschema.validate(xml_tree):
-    print("XML is valid against XSD.")
-else:
-    print("XML is not valid against XSD.")
-    exit(1)
+    # Load XML and XSL files
+    xml_tree = etree.parse(xml_file)
+    xsl_tree = etree.parse(xsl_file)
 
-# Write transformed HTML to a file
-with open("output.html", "wb") as output_file:
-    output_file.write(etree.tostring(html_tree, pretty_print=True))
+    # Apply transformation
+    transform = etree.XSLT(xsl_tree)
+    html_result = transform(xml_tree)
+
+    # Save the HTML result to a file
+    output_filename = "Web Stack\Assignment 5\output.html"
+    with open(output_filename, "wb") as html_file:
+        html_file.write(etree.tostring(html_result, pretty_print=True))
+    print(f"XML to HTML transformation complete. HTML saved to {output_filename}")
+                
+
+except Exception as e:
+        print("Error!")
+        print(e)
+
+
+
+
