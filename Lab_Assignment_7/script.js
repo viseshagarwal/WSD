c0fce9e67039434a187845588c0ab3d624cc6aa3
viseshagarwal <viseshagarwal@outlook.com>
2023-09-12 15:22:24 +0530

    edited few files

--- Lab_Assignment_7/script.js
+++ Lab_Assignment_7/script.js
@@ -1,33 +1,34 @@
 function fetchAndDisplayBooks() {
-    const bookList = document.getElementById("bookList");
-    const xhr = new XMLHttpRequest();
-    const url = "books.json";
-    xhr.open("GET", url, true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            const books = JSON.parse(xhr.responseText);
+  const bookList = document.getElementById("bookList");
+  const xhr = new XMLHttpRequest();
+  const url = "https://elibraryrestapi.herokuapp.com/elibrary/api/book/list";
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const books = JSON.parse(xhr.responseText);
 
-            displayBooks(books);
-        } else {
-            bookList.innerHTML = "Failed to fetch books.";
-        }
-    };
-    xhr.send();
+      displayBooks(books);
+    } else {
+      bookList.innerHTML = "Failed to fetch books.";
+    }
+  };
+  xhr.send();
 }
 
 function displayBooks(books) {
-    const bookList = document.getElementById("bookList");
+  const bookList = document.getElementById("bookList");
 
-    
-    bookList.innerHTML = "";
-    const ul = document.createElement("ul");
+  bookList.innerHTML = "";
+  const ul = document.createElement("ul");
 
-    books.forEach(function (book) {
-        const li = document.createElement("li");
-        li.textContent = book.title;
-        ul.appendChild(li);
-    });
+  books.forEach(function (book) {
+    const li = document.createElement("li");
+    li.textContent = book.title;
+    ul.appendChild(li);
+  });
 
-    bookList.appendChild(ul);
+  bookList.appendChild(ul);
 }
-document.getElementById("fetchBooks").addEventListener("click", fetchAndDisplayBooks);
+document
+  .getElementById("fetchBooks")
+  .addEventListener("click", fetchAndDisplayBooks);
