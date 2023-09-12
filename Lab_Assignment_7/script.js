711349d526c4e42bdad075a008a2126d85881fc3
viseshagarwal <viseshagarwal@outlook.com>
2023-09-12 15:14:45 +0530

    updated few files

---
+++ Lab_Assignment_7/script.js
@@ -0,0 +1,33 @@
+function fetchAndDisplayBooks() {
+    const bookList = document.getElementById("bookList");
+    const xhr = new XMLHttpRequest();
+    const url = "books.json";
+    xhr.open("GET", url, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const books = JSON.parse(xhr.responseText);
+
+            displayBooks(books);
+        } else {
+            bookList.innerHTML = "Failed to fetch books.";
+        }
+    };
+    xhr.send();
+}
+
+function displayBooks(books) {
+    const bookList = document.getElementById("bookList");
+
+    
+    bookList.innerHTML = "";
+    const ul = document.createElement("ul");
+
+    books.forEach(function (book) {
+        const li = document.createElement("li");
+        li.textContent = book.title;
+        ul.appendChild(li);
+    });
+
+    bookList.appendChild(ul);
+}
+document.getElementById("fetchBooks").addEventListener("click", fetchAndDisplayBooks);
