953d8a591b3d4ff5b1b64baaa54f82a285b4f085
viseshagarwal <viseshagarwal@outlook.com>
2023-09-14 23:32:24 +0530

    edited few files

--- Lab_Assignment_7/script.js
+++ Lab_Assignment_7/script.js
@@ -1,3 +1,4 @@
+/*
 function fetchAndDisplayBooks() {
   const bookList = document.getElementById("bookList");
   const xhr = new XMLHttpRequest();
@@ -32,3 +33,59 @@ function displayBooks(books) {
 document
   .getElementById("fetchBooks")
   .addEventListener("click", fetchAndDisplayBooks);
+*/
+document.addEventListener("DOMContentLoaded", function () {
+  const fetchBooksButton = document.getElementById("fetchBooks");
+  const bookListDiv = document.getElementById("bookList");
+
+  fetchBooksButton.addEventListener("click", function () {
+      // Create a new XMLHttpRequest object
+      const xhr = new XMLHttpRequest();
+
+      // Define the URL for the JSON data (replace with your API URL)
+      const apiUrl = "https://example.com/api/books.json";
+
+      // Configure the AJAX request
+      xhr.open("GET", apiUrl, true);
+
+      // Set the response type to JSON
+      xhr.responseType = "json";
+
+      // Define what happens when the request is successful
+      xhr.onload = function () {
+          if (xhr.status === 200) {
+              const books = xhr.response;
+              displayBooks(books);
+          } else {
+              // Handle the error
+              console.error("Error fetching books:", xhr.status);
+          }
+      };
+
+      // Handle network errors
+      xhr.onerror = function () {
+          console.error("Network error while fetching books");
+      };
+
+      // Send the AJAX request
+      xhr.send();
+  });
+
+  function displayBooks(books) {
+      // Clear the existing book list
+      bookListDiv.innerHTML = "";
+
+      // Create an unordered list to display the books
+      const ul = document.createElement("ul");
+
+      // Iterate through the list of books and create list items
+      books.forEach(function (book) {
+          const li = document.createElement("li");
+          li.textContent = `${book.title} by ${book.author}`;
+          ul.appendChild(li);
+      });
+
+      // Append the list to the bookListDiv
+      bookListDiv.appendChild(ul);
+  }
+});
