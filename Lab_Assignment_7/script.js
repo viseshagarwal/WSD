564690ec34826657a08bb09d82a5edc27b4b0577
viseshagarwal <viseshagarwal@outlook.com>
2023-09-14 23:34:08 +0530

    commit

--- Lab_Assignment_7/script.js
+++ Lab_Assignment_7/script.js
@@ -39,53 +39,53 @@ document.addEventListener("DOMContentLoaded", function () {
   const bookListDiv = document.getElementById("bookList");
 
   fetchBooksButton.addEventListener("click", function () {
-      // Create a new XMLHttpRequest object
-      const xhr = new XMLHttpRequest();
+    // Create a new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
 
-      // Define the URL for the JSON data (replace with your API URL)
-      const apiUrl = "https://example.com/api/books.json";
+    // Define the URL for the JSON data (replace with your API URL)
+    const apiUrl = "books.json";
 
-      // Configure the AJAX request
-      xhr.open("GET", apiUrl, true);
+    // Configure the AJAX request
+    xhr.open("GET", apiUrl, true);
 
-      // Set the response type to JSON
-      xhr.responseType = "json";
+    // Set the response type to JSON
+    xhr.responseType = "json";
 
-      // Define what happens when the request is successful
-      xhr.onload = function () {
-          if (xhr.status === 200) {
-              const books = xhr.response;
-              displayBooks(books);
-          } else {
-              // Handle the error
-              console.error("Error fetching books:", xhr.status);
-          }
-      };
+    // Define what happens when the request is successful
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const books = xhr.response;
+        displayBooks(books);
+      } else {
+        // Handle the error
+        console.error("Error fetching books:", xhr.status);
+      }
+    };
 
-      // Handle network errors
-      xhr.onerror = function () {
-          console.error("Network error while fetching books");
-      };
+    // Handle network errors
+    xhr.onerror = function () {
+      console.error("Network error while fetching books");
+    };
 
-      // Send the AJAX request
-      xhr.send();
+    // Send the AJAX request
+    xhr.send();
   });
 
   function displayBooks(books) {
-      // Clear the existing book list
-      bookListDiv.innerHTML = "";
+    // Clear the existing book list
+    bookListDiv.innerHTML = "";
 
-      // Create an unordered list to display the books
-      const ul = document.createElement("ul");
+    // Create an unordered list to display the books
+    const ul = document.createElement("ul");
 
-      // Iterate through the list of books and create list items
-      books.forEach(function (book) {
-          const li = document.createElement("li");
-          li.textContent = `${book.title} by ${book.author}`;
-          ul.appendChild(li);
-      });
+    // Iterate through the list of books and create list items
+    books.forEach(function (book) {
+      const li = document.createElement("li");
+      li.textContent = `${book.title} by ${book.author}`;
+      ul.appendChild(li);
+    });
 
-      // Append the list to the bookListDiv
-      bookListDiv.appendChild(ul);
+    // Append the list to the bookListDiv
+    bookListDiv.appendChild(ul);
   }
 });
