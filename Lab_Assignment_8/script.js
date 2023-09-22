053701eeca8bf069e80feb5310fc326e256a0e78
viseshagarwal <viseshagarwal@outlook.com>
2023-09-22 19:48:02 +0530

    Made new changes

--- Lab_Assignment_8/script.js
+++ Lab_Assignment_8/script.js
@@ -1,11 +1,3 @@
-/*
-Write JavaScript code to make an AJAX request to fetch the products.json file from the remote server.
-Data Display:
-Create a function to parse the JSON data once it's retrieved successfully.
-Dynamically generate HTML elements (e.g., divs, images, and paragraphs) for each product in the catalog.
-Display the product information on the webpage, including product names, descriptions, prices, and images.
-*/
-
 function callAPI() {
   // let httpRequest;
   const httpRequest = new XMLHttpRequest();
@@ -13,50 +5,56 @@ function callAPI() {
   makeRequest(url);
 
   function makeRequest(url) {
-    httpRequest.onreadystatechange = alertContents;
+    httpRequest.onreadystatechange = writeContents;
     httpRequest.open("GET", url, true);
     httpRequest.send();
   }
 
-  function alertContents() {
+  function writeContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         const response = JSON.parse(httpRequest.responseText);
-        console.log(response);
+        //console.log(response);
         response.products.map(
           (product) =>
             (document.getElementById("display").innerHTML += `
             <div class="col-sm-6 col-md-4 col-lg-3">
             <div class="card ml-4" style="width: 18rem;">
-              <img src="${product.images[0]}" class="card-img-top" alt="...">
+              <img src="${product.thumbnail}"  width="286px" height="280px" >
               <div class="card-body">
-                <h3>${product.title}</h3>
+                <h3 class="newWorld">${product.title}</h3>
+                <h5>${product.brand}</h5>
                   <p class="card-text">${product.description}</p>
                   <p class="card-text"> $ ${product.price}</p>
-                  <a href="${product.link}">book link</a>
+                  <p class="card-text">Rating : ${product.rating}</p>
+                  
       </div>
   </div>
     </div>`)
         );
+        // fucntion to search the product
       } else {
         alert("There was a problem with the request.");
       }
     }
   }
 
-  console.log("hello", httpRequest.responseText);
+  //console.log("hello", httpRequest.responseText);
 }
 window.onload = callAPI();
 
-document.getElementById("searchbox").onkeypress = function () {
-  search();
-};
-
-// Seach bar
-function search() {
+// Search function using H3 tag
+function searchProduct() {
   let input = document.getElementById("search").value;
   input = input.toLowerCase();
-  let x = document.getElementsByTagName("h3");
+  let x = document.getElementsByClassName("newWorld");
   console.log(x);
-  console.log(input);
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].parentElement.parentElement.parentElement.style.display = "none";
+    } else {
+      x[i].parentElement.parentElement.parentElement.style.display =
+        "inline-block";
+    }
+  }
 }
