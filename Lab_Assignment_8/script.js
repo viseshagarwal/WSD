24d4d252814ebf5f54a46a9417b123960d3d8d29
viseshagarwal <viseshagarwal@outlook.com>
2023-09-29 11:51:47 +0530

    made some changes in the existing files

--- Lab_Assignment_8/script.js
+++ Lab_Assignment_8/script.js
@@ -1,60 +1,79 @@
-function callAPI() {
-  // let httpRequest;
-  const httpRequest = new XMLHttpRequest();
-  const url = "https://dummyjson.com/products";
-  makeRequest(url);
+document.addEventListener("DOMContentLoaded", function () {
+  const productList = document.getElementById("productList");
+  const searchInput = document.getElementById("searchInput");
+  const sortSelect = document.getElementById("sortSelect");
+  const notFoundMessage = document.getElementById("notFoundMessage");
 
-  function makeRequest(url) {
-    httpRequest.onreadystatechange = writeContents;
-    httpRequest.open("GET", url, true);
-    httpRequest.send();
-  }
+  function fetchProducts() {
+    const xhr = new XMLHttpRequest();
+    const url =
+      "https://raw.githubusercontent.com/viseshagarwal/WSD/main/Lab_Assignment_8/items.json";
 
-  function writeContents() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        const response = JSON.parse(httpRequest.responseText);
-        //console.log(response);
-        response.products.map(
-          (product) =>
-            (document.getElementById("display").innerHTML += `
-            <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card ml-4" style="width: 18rem;">
-              <img src="${product.thumbnail}"  width="286px" height="280px" >
-              <div class="card-body">
-                <h3 class="newWorld">${product.title}</h3>
-                <h5>${product.brand}</h5>
-                  <p class="card-text">${product.description}</p>
-                  <p class="card-text"> $ ${product.price}</p>
-                  <p class="card-text">Rating : ${product.rating}</p>
-                  
-      </div>
-  </div>
-    </div>`)
-        );
-        // fucntion to search the product
-      } else {
-        alert("There was a problem with the request.");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          const products = JSON.parse(xhr.responseText);
+          const keyword = searchInput.value.toLowerCase().trim();
+          const filteredProducts = products.filter(
+            (product) =>
+              product.name.toLowerCase().includes(keyword) ||
+              product.description.toLowerCase().includes(keyword)
+          );
+
+          const sortBy = sortSelect.value;
+          filteredProducts.sort((a, b) => {
+            if (sortBy === "name") {
+              return a.name.localeCompare(b.name);
+            } else if (sortBy === "price") {
+              return a.price - b.price;
+            }
+          });
+
+          if (filteredProducts.length === 0) {
+            productList.innerHTML = "";
+            notFoundMessage.textContent = "No products found.";
+            return;
+          }
+
+          notFoundMessage.textContent = "";
+          productList.innerHTML = "";
+
+          filteredProducts.forEach((product) => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("product");
+
+            const productName = document.createElement("h2");
+            productName.textContent = product.name;
+
+            const productDescription = document.createElement("p");
+            productDescription.textContent = product.description;
+
+            const productPrice = document.createElement("p");
+            productPrice.textContent = `Price: ₹${product.price.toFixed(2)}`;
+
+            const productImage = document.createElement("img");
+            productImage.src = product.image_url;
+            productImage.alt = product.name;
+
+            productDiv.appendChild(productImage);
+            productDiv.appendChild(productName);
+            productDiv.appendChild(productDescription);
+            productDiv.appendChild(productPrice);
+
+            productList.appendChild(productDiv);
+          });
+        } else {
+          console.error("Error fetching data. Status:", xhr.status);
+        }
       }
-    }
+    };
+
+    xhr.open("GET", url, true);
+    xhr.send();
   }
 
-  //console.log("hello", httpRequest.responseText);
-}
-window.onload = callAPI();
+  fetchProducts();
 
-// Search function using H3 tag
-function searchProduct() {
-  let input = document.getElementById("search").value;
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName("newWorld");
-  console.log(x);
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-      x[i].parentElement.parentElement.parentElement.style.display = "none";
-    } else {
-      x[i].parentElement.parentElement.parentElement.style.display =
-        "inline-block";
-    }
-  }
-}
+  searchInput.addEventListener("input", fetchProducts);
+  sortSelect.addEventListener("change", fetchProducts);
+});
